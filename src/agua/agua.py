aed4169a35aa48c153fdf630fdf6b08f85334855
joaocapechi <joaopedrocapechi@gmail.com>
2026-06-05 17:08:36 -0300

    Func BFS água

--- src/agua/agua.py
+++ src/agua/agua.py
@@ -1,6 +1,10 @@
 __all__ = ["cria_mascara_agua", "expandir_mascara_agua"]
 
+# Import das bibliotecas necessárias
+import numpy as np
 
+
+# Funções
 def cria_mascara_agua(tam_x: int, tam_y: int, xy_fonte: int) -> list[list[int]]:
     '''
         Cria a máscara de água com nascente em um dos cantos
@@ -18,7 +22,7 @@ def cria_mascara_agua(tam_x: int, tam_y: int, xy_fonte: int) -> list[list[int]]:
     if not fonteValida:
         return [3]
     
-    ret = [
+    _matrizAgua = [
         [0] * tam_y
         for _ in range(tam_x)
     ]
@@ -31,9 +35,58 @@ def cria_mascara_agua(tam_x: int, tam_y: int, xy_fonte: int) -> list[list[int]]:
     }
 
     linha, col = pos_fonte[xy_fonte]
-    ret[linha][col] = 1
+    _matrizAgua[linha][col] = 1
     
-    return ret
+    return _matrizAgua
 
-def expandir_mascara_agua(terreno, masc_agua: list[list[int]], nivel_do_mar: int) -> float:
-    return
+
+def expandir_mascara_agua(terreno: np.ndarray, masc_agua: list[list[int]], nivel_do_mar: int) -> float:
+    if nivel_do_mar <= 0:
+        return -2.0
+    
+    tam_x: int = len(masc_agua)
+    tam_y: int = len(masc_agua[0])
+    
+    has_same_size: bool = (tam_x, tam_y) == terreno.shape
+    if not has_same_size:
+        return -1.0
+
+    # Para o BFS
+    neighbors: list[tuple[int, int]] = []
+    visited: list[bool] = [
+        [False] * tam_y
+        for _ in range(tam_y)
+    ]
+    qtd: int = 0
+
+    # Achar a posição em que a nascente d`água está
+    pos_fonte = {
+        0: (0,0),
+        1: (0, tam_y-1),
+        2: (tam_x - 1, tam_y - 1),
+        3: (tam_x - 1, 0)
+    }
+
+    for _, (x, y) in pos_fonte.items():
+        if masc_agua[x][y] == 1:
+            neighbors.append((x,y))
+            break
+
+    while bool(neighbors):
+        x, y = neighbors.pop(0)
+        if visited[x][y]:
+            continue
+
+        visited[x][y] = True
+        masc_agua[x][y] = 1
+        qtd += 1
+
+        for new_x in range(max(0,x-1),min(tam_x, x+2)):
+            for new_y in range(max(0, y-1), min(tam_y, y+2)):
+                if new_x == x and new_y == y:
+                    continue
+                if (not visited[new_x][new_y]) and (terreno[new_x][new_y] <= nivel_do_mar):
+                    neighbors.append((new_x, new_y))
+
+    
+    return float(qtd/(tam_x * tam_y) * 100)
--- src/agua/teste_agua.py
+++ src/agua/teste_agua.py
@@ -1,7 +1,11 @@
 import unittest
-from agua.agua import cria_mascara_agua, expandir_mascara_agua
+import numpy as np
+
+from agua import cria_mascara_agua, expandir_mascara_agua
+
 
 class TesteCriaMascaraAgua(unittest.TestCase):
+    # ---------- cria_mascara_agua ----------
     '''
         Casos que os inputs são válidos
     '''
@@ -34,8 +38,74 @@ class TesteCriaMascaraAgua(unittest.TestCase):
     def test_InvalidWaterFountain_2(self):
         self.assertEqual(cria_mascara_agua(10, 10, 4), [3])
 
+    
+    # ---------- expandir_mascara_agua ----------
+    '''
+        Casos que os inputs são válidos
+    '''
+    def test_ValidInputs1(self):
+        terreno = np.array(
+            [
+                [5, 3, 2],
+                [10, 2, 0],
+                [1, 0, 0]
+            ], dtype=float
+        )
+
+        self.assertEqual(expandir_mascara_agua(terreno, cria_mascara_agua(3,3,2), 3), 7/9 * 100)
+    def test_ValidInputs2(self):
+        terreno = np.array(
+            [
+                [5, 1, 2],
+                [1, 4, 10],
+                [1, 10, 0]
+            ], dtype=float
+        )
+
+        self.assertEqual(expandir_mascara_agua(terreno, cria_mascara_agua(3,3,2), 3), 1/9 * 100)
+    
+    '''
+        Casos em que o tamanho das matrizes é diferente
+    '''
+    def test_DiferentSizeMat1(self):
+        terreno = np.array(
+            [
+                [5, 1, 2],
+                [1, 4, 10],
+                [1, 10, 0]
+            ], dtype=float
+        )
+
+        self.assertEqual(expandir_mascara_agua(terreno, cria_mascara_agua(2,2,2), 3), -1.0)
+    def test_DiferentSizeMat2(self):
+        terreno = np.array(
+            [
+                [5, 1, 2, 3],
+                [1, 4, 10, 3],
+                [1, 10, 0, 3]
+            ], dtype=float
+        )
+
+        self.assertEqual(expandir_mascara_agua(terreno, cria_mascara_agua(3,3,2), 3), -1.0)
+
+    '''
+        Caso em que o nível do mar é nulo
+    '''
+    def test_NivelNulo1(self):
+        terreno = np.array(
+            [
+                [5, 1, 2],
+                [1, 4, 10],
+                [1, 10, 0]
+            ], dtype=float
+        )
+
+        self.assertEqual(expandir_mascara_agua(terreno, cria_mascara_agua(3,3,2), 0), -2.0)
+
+
 def main():
     unittest.main()
+
 
 if __name__ == "__main__":
     main()
